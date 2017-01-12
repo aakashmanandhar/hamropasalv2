bc9f8d21c2f163648298024a239f76fd9af32c49
Aakash Manandhar <aakashmanandhar@gmail.com>
2017-01-13 02:21:35 +0545

    Django Session II Base.html plus Template Adding

--- category/views.py
+++ category/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Category
 
 # Create your views here.
+
+def home_page(request):
+    categories = Category.objects.all()
+    print(categories)
+    return render(request, 'homepage.html', {'categories':categories})
+    
