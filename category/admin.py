80c8f7b68740b1b2f3d2f952f5ab1cbf6c5b8930
Aakash Manandhar <aakashmanandhar@gmail.com>
2017-01-14 17:42:45 +0545

    Django Session III Foreign Key Relationship Concept

--- category/admin.py
+++ category/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Product
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'created_on', 'updated_on', )
+    list_display = ('name', 'description', 'created_on', 'updated_on', 'category_id' )
     list_filter = ('name', )
-    fields = ('name', )
+    fields = ('name', 'category_id')
     search_fields = ('name', )
 
 admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_on', 'updated_on', 'category', )
+
+admin.site.register(Product, ProductAdmin)
--- category/models.py
+++ category/models.py
@@ -6,11 +6,22 @@ class Category(models.Model):
     description = models.TextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on= models.DateTimeField(auto_now=True)
+    category_id=models.ForeignKey('self', blank=True, null=True)
     
     def __str__(self):
         return(self.name)
     
     def __repr__(self):
         return(self.name)
-    
-    
+
+class Product(models.Model):
+    name = models.CharField(max_length=32)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, blank=True, null=True)
+
+    def __str__(self):
+        return(self.name)
+
+    def __repr__(self):
+        return(self.name)
